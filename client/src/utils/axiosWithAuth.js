c0aa2f734eb9f457fd70623a96cf2615da8bc0b2
Ruth Poliakon <r.poliakon@gmail.com>
2019-11-08 17:33:48 -0700

    BubblePage private route

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-export function getToken() {
-  return localStorage.getItem("token");
-}
-
 export default function() {
   return axios.create({
     baseURL: "http://localhost:5000/api",
